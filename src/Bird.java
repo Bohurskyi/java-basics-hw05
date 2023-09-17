2b304f0197fc2e67348e030a08596515911bc7ca
Dmytro Bohurskyi <bogurskiy96@gmail.com>
2023-09-17 22:28:24 +0300

    Initial commit

---
+++ src/Bird.java
@@ -0,0 +1,8 @@
+public abstract class Bird {
+  // TODO fix class declaration
+
+  public void eat() {
+    // TODO print a message that bird is eating
+    System.out.println("Bird is eating");
+  }
+}
